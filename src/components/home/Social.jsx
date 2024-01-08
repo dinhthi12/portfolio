b81990a51688959f9e3103758c44915b212ed87d
Đình Thi <73831236+dinhthi12@users.noreply.github.com>
2024-01-08 17:01:04 +0700

    Home Component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/nguyen-dinh-thi-819471214/"
+        className="home__social-icon"
+        target="_blank" rel="noreferrer"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/dinhthi12"
+        className="home__social-icon"
+        target="_blank" rel="noreferrer"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UCK9nL1AikfpMvRTwed0NDxQ"
+        className="home__social-icon"
+        target="_blank" rel="noreferrer"
+      >
+        <i className="uil uil-youtube"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
