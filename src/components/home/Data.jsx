a1f5df444b49e01a1af2beeab20610b53f401739
Đình Thi <73831236+dinhthi12@users.noreply.github.com>
2024-01-08 22:29:54 +0700

    About Component

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,7 +4,7 @@ const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
-        Nguyen Dinh Thi
+        NGUYEN DINH THI
         <svg
           width="36"
           height="36"
